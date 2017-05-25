b4d77ae0477a08e8b1769035825570f4b91f50bb
Quinn McHenry <quinn@jqm.us>
2017-05-25 16:59:24 -0400

    Add ability to use stdout when not specifying output file

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,16 +4,6 @@
 // Converts a YAML input file through a Stencil template to make shiny happy strings
 //
 
-//    Usage: genstrings [options]
-//    -i, --input:
-//        Input yaml file
-//    -t, --template:
-//        Stencil template file
-//    -o, --output:
-//        Output file
-//    -c, --compare:
-//        Files to compare modification dates against (multiple values separated by space)
-
 import CommandLineKit
 import FileKit
 import Foundation
@@ -26,8 +16,8 @@ let inputPath = StringOption(shortFlag: "i", longFlag: "input", required: true,
                              helpMessage: "Input yaml file")
 let templatePath = StringOption(shortFlag: "t", longFlag: "template", required: true,
                              helpMessage: "Stencil template file")
-let outputPath = StringOption(shortFlag: "o", longFlag: "output", required: true,
-                             helpMessage: "Output file")
+let outputPath = StringOption(shortFlag: "o", longFlag: "output", required: false,
+                             helpMessage: "Output file (writes to stdout if not provided)")
 let comparePaths = MultiStringOption(shortFlag: "c", longFlag: "compare", required: false,
                              helpMessage: "Files to compare modification dates against (multiple values separated by space)")
 
@@ -50,11 +40,6 @@ guard let templateFile = templatePath.value else {
   exit(EX_USAGE)
 }
 
-guard let outputFile = outputPath.value else {
-  print("Error understanding output file path")
-  exit(EX_USAGE)
-}
-
 guard let yaml = CodeGen.loadYAML(fromFile: inputFile)  else {
   print("Error reading input YAML file")
   exit(EX_DATAERR)
@@ -62,28 +47,32 @@ guard let yaml = CodeGen.loadYAML(fromFile: inputFile)  else {
 
 
 let input = Path(inputFile)
-let output = Path(outputFile)
+let output = outputPath.value.map{ Path($0) }
 let template = Path(templateFile)
 
 
-// check if any of the optional compare files are newer than output file, if so, force the template generation
+var performOperation: Bool {
+    guard let output = output else { return true }
+    
+    // check if any of the optional compare files are newer than output file, if so, force the template generation
 
-let compareFilesNewer = comparePaths.value?.map{ Path($0) }
-                                           .map{ $0.modified(since: output) }
-                                           .reduce(false) { $0 || $1 }
+    let compareFilesNewer = comparePaths.value?.map{ Path($0) }
+                                               .map{ $0.modified(since: output) }
+                                               .reduce(false) { $0 || $1 } ?? false
 
-let inputNewer = input.modified(since: output)
-let templateNewer = template.modified(since: output)
+    let inputNewer = input.modified(since: output)
+    let templateNewer = template.modified(since: output)
 
-// Run through the template rendering if any
-// of the following conditions are met:
-//      1. Source file is newer than the output files  OR
-//      2. Template file is newer than output file     OR
-//      3. Optional compare files are newer than output file OR
-//      4. Output file doesn't exist.
+    // Run through the template rendering if any
+    // of the following conditions are met:
+    //      1. Source file is newer than the output files  OR
+    //      2. Template file is newer than output file     OR
+    //      3. Optional compare files are newer than output file OR
+    //      4. Output file doesn't exist.
+    return inputNewer || templateNewer || compareFilesNewer
+}
 
-if !output.exists || inputNewer || templateNewer || compareFilesNewer ?? false {
-
+if output == nil || performOperation {
 
   // Convert YAML data into standard Swift types Array/Dictionary/String/...
   guard let dataDictionary = yaml.convert() as? [String: Any] else {
@@ -96,15 +85,19 @@ if !output.exists || inputNewer || templateNewer || compareFilesNewer ?? false {
     let templateContent = try String(contentsOfFile: templateFile, encoding: String.Encoding.utf8)
     let template = try Template(string: templateContent)
     let rendered = try template.render(with: Box(dataDictionary))
-    print("\(output.exists ? "Regenerating" : "Creating") output file: \(output.fileName)")
-    try TextFile(path: output).write(rendered, atomically: true)
+    if let output = output {
+        print("\(output.exists ? "Regenerating" : "Creating") output file: \(output.fileName)")
+        try TextFile(path: output).write(rendered, atomically: true)
+    } else {
+        print(rendered)
+    }
   } catch let error as MustacheError {
     print(error.description)
     exit(EX_DATAERR)
   }
 
 } else {
-  print("Skipping \(output.fileName)")
+  print("Skipping \(output?.fileName ?? input.fileName)")
 }
 
 exit(EX_OK)
