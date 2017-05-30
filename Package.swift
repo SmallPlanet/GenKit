ca1377dee60bf1d667b496c8d95bb2cd242ce4ae
Quinn McHenry <quinn@jqm.us>
2017-05-30 16:44:53 -0400

    Remove unused package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     dependencies: [
 	.Package(url: "https://github.com/jatoben/CommandLine.git", Version(3, 0, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
         .Package(url: "https://github.com/behrang/YamlSwift.git", Version(3, 3, 1)),
-        .Package(url: "https://github.com/yaslab/CSV.swift.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/IBM-Swift/GRMustache.swift", Version(1, 7, 2)),
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/nvzqz/FileKit", Version(4, 0, 1)),
