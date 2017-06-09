16b9ca1c98423f59d1cdabd42fb2a1cb696e06df
Quinn McHenry <quinn@jqm.us>
2017-06-09 17:17:48 -0400

    Add optional metadictionary in generate functions
    If the "GenKit" key is absent in the dictionary, the generate
    functions will add a dictionary with that key for the template to
    use that include metainformation like `datetime` which contains
    the time the tool was run and `templatePath` (if available)
    that describes the file used for the template content. These can
    be accessed from a template as `{{ GenKit.datetime }}` which may
    be useful in comments in generated code.
    
    Sending a dictionary as metaDictionary in the generate functions
    will insert the contents to the autogenerated dictionary GenKit
    adds.

--- Sources/GenKit/GenKit.swift
+++ Sources/GenKit/GenKit.swift
@@ -4,37 +4,50 @@ import Stencil
 import Yaml
 
 public struct GenKit {
-
+    
     public enum Template {
         case mustache
         case stencil
     }
-
+    
     public static func loadYAML(fromFile file: String) throws -> Any {
         let yamlString = try String(contentsOf: URL(fileURLWithPath: file), encoding: .utf8)
         let yaml = try Yaml.load(yamlString)
         return yaml.toAny()
     }
-
+    
     public static func generate(
         _ input: [String: Any],
         templateFile file: String,
-        template: Template = .mustache
+        template: Template = .mustache,
+        metaDictionary meta: [String: Any] = [:]
     ) throws -> String {
-        var dataDictionary = input
-
-        // add a dictionary with meta information only if the key does not already exist
-        if !dataDictionary.contains(where: { $0.0 == "GenKit" }) {
-            let meta: [String:Any] = [
-                "datetime": Date().description,
-                "templatePath": file
-            ]
-            dataDictionary["GenKit"] = meta
-        }
-
-        let rendered: String
         let templateContent = try String(contentsOfFile: file, encoding: String.Encoding.utf8)
-
+        var meta = meta
+        meta["templatePath"] = file
+        return try generate(input,
+                            templateString: templateContent,
+                            template: template,
+                            metaDictionary: meta)
+    }
+    
+    public static func generate(
+        _ input: [String: Any],
+        templateString templateContent: String,
+        template: Template = .mustache,
+        metaDictionary meta: [String: Any] = [:]
+    ) throws -> String {
+        var input = input
+        var meta = meta
+        
+        // add a dictionary with meta information only if the key does not already exist
+        if !input.contains(where: { $0.0 == "GenKit" }) {
+            meta["datetime"] = Date().description
+            input["GenKit"] = meta
+        }
+        
+        let rendered: String
+        
         switch template {
         case .mustache(_):
             let template = try Mustache.Template(string: templateContent)
@@ -42,8 +55,8 @@ public struct GenKit {
         case .stencil(_):
             rendered = try Environment().renderTemplate(string: templateContent, context: input)
         }
-
+        
         return rendered
     }
-
+    
 }
