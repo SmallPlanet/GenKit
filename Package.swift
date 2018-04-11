a29ec0f196acc21f8b8ac86e13a44783e693dd4c
Teju Prasad <skwadim@yahoo.com>
2018-04-11 14:33:39 -0400

    Another change for Xcode 9.3 compatibility

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
 	.Package(url: "https://github.com/jatoben/CommandLine.git", Version(3, 0, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
         .Package(url: "https://github.com/behrang/YamlSwift.git", Version(3, 3, 1)),
-        .Package(url: "https://github.com/groue/GRMustache.git", Version(7, 3, 2)),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", Version(1, 7, 3)),
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/SmallPlanetSporks/FileKit", Version(4, 0, 2)),
     ]
