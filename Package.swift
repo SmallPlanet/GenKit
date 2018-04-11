14260425b561721212b3f9b9f8fa7c5863c8cc7c
Teju Prasad <skwadim@yahoo.com>
2018-04-11 15:07:31 -0400

    Update YAMLSwift version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     targets: [Target(name: "gen", dependencies: ["GenKit"]),],
     dependencies: [
 	.Package(url: "https://github.com/jatoben/CommandLine.git", Version(3, 0, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
-        .Package(url: "https://github.com/behrang/YamlSwift.git", Version(3, 3, 1)),
+        .Package(url: "https://github.com/behrang/YamlSwift.git", Version(3, 4, 3)),
         .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", Version(1, 7, 3)),
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/SmallPlanetSporks/FileKit", Version(4, 0, 2)),
